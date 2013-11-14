e7c77f5a55a5f784025876b79cb417840953e99f
Gordon Williams <gw@pur3.co.uk>
2013-11-14 14:18:50 +0000

    now use closure compiler to provide minified versions of libraries

--- build.sh
+++ build.sh
@@ -16,6 +16,30 @@ cp datasheets/* $WEBSITE/www/datasheets
 mkdir -p $WEBSITE/www/refimages
 rm $WEBSITE/www/refimages/*
 cp html/refimages/* $WEBSITE/www/refimages
-mkdir -p $WEBSITE/www/modules
-rm $WEBSITE/www/modules/*
-cp devices/*.js $WEBSITE/www/modules
+
+MODULEDIR=$WEBSITE/www/modules
+mkdir -p $MODULEDIR
+rm $MODULEDIR/*
+cp devices/*.js $MODULEDIR
+
+# Minify all modules
+for module in `ls $MODULEDIR/*.js`; do
+  BNAME=`basename $module .js`
+  MINJS=${BNAME}.min.js
+  echo Minifying $MODULEDIR/$module to $MINJS
+  
+  # Minify, and convert binary to a proper number
+  sed "s/0b\([01][01]*\)/parseInt(\"\1\",2)/g" $module | curl -s \
+    -d compilation_level=SIMPLE_OPTIMIZATIONS \
+    -d output_format=text \
+    -d output_info=compiled_code \
+    --data-urlencode "js_code@/dev/fd/0" \
+   http://closure-compiler.appspot.com/compile > $MODULEDIR/$MINJS
+
+   if [[ -s $MODULEDIR/$MINJS ]] ; then 
+     echo "$MODULEDIR/$MINJS compile successful"
+   else
+     echo "$MODULEDIR/$MINJS compile FAILED."
+     exit 1
+   fi ;
+done
