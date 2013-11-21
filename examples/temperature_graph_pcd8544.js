c68ba7a91ab54c234e5fd793786f88e9700f48e4
Gordon Williams (u36) <gw@pur3.co.uk>
2013-11-21 17:28:43 +0000

    another example

--- examples/temperature_graph_pcd8544.js
+++ examples/temperature_graph_pcd8544.js
@@ -1,8 +1,11 @@
 /* 
-* KEYWORDS: PCD8544,DS18B20,LCD,Temperature,Graphics,Font,Text
+Temperature Graph on a PCD8544 display, with DS18B20 tempetarure sensor
+=======================================================================
+
+* KEYWORDS: PCD8544,DS18B20,LCD,Temperature,Graphics,Graph
 
    Using the Nokia 5110 display and the DS18B20 temperature
-   sensor to make a simple temperature display 
+   sensor to make a simple temperature graph 
 
 | LCD pin | Pin on Espruino Board         |
 |---------|-------------------------------|
@@ -27,17 +30,23 @@ SPI1.setup({ baud: 1000000, sck:B3, mosi:B5 });
 var g = require("PCD8544").connect(SPI1,B6,B7,B8);
 var temp = require("DS18B20").connect(B13);
 
+// create history points, one for each pixel on the LCD
+var history = new Int8Array(g.getWidth());
+
 setInterval(function() {
   // Get the temperature
   var t = temp.getTemp();
-  // Round it to the nearest 0.1
-  t = Math.round(t*10)/10;
-  // Now draw!
+  // Move our history back and add our new temperature at the end
+  for (var i=1;i<history.length;i++)
+    history[i-1] = history[i];
+  history[history.length-1] = t;
+  // function to convert the temperature to a position on the screen
+  var tempToY = function(t) { return 30-t; };
+  // draw the graph!
   g.clear();
-  g.setFontBitmap(); // simple 8x8 font
-  g.drawString("Temp",0,0);
-  g.drawLine(0,10,84,10);
-  g.setFontVector(25); // large font
-  g.drawString(t, 0, 15);
-  g.flip(); // copy this to the screen
-}, 200);
+  g.moveTo(0,tempToY(history[0]));
+  for (i=1;i<history.length;i++)
+    g.lineTo(i,tempToY(history[i]));
+  // display on the screen
+  g.flip();
+}, 500);
