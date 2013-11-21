fc57f6ae4834fc0e49e82b315a5e0141a63c766b
Gordon Williams (u36) <gw@pur3.co.uk>
2013-11-21 18:17:39 +0000

    update HD44780 display driver

---
+++ devices/HD44780.js
@@ -0,0 +1,76 @@
+/* Copyright (c) 2013 Gordon Williams, Pur3 Ltd. See the file LICENSE for copying permission. */
+/* 
+Module for the HD44780 controller in text-based LCDs (pretty much all 16x2 and 20x4 LCDs)
+
+If you have one of the LCDs with an I2C backpack, just use:
+
+```
+I2C1.setup({scl:B6, sda:B7});
+var lcd = require("HD44780").connectI2C(I2C1);
+lcd.print("Hello World!");
+```
+
+Otherwise try:
+
+```
+var lcd = require("HD44780").connect(A4,A5,A0,A1,A2,A3);
+lcd.print("Hello World!");
+```  
+*/
+
+function HD44780(write) {
+ // initialise
+ write(0x33,1);
+ write(0x32,1);
+ write(0x28,1);
+ write(0x0C,1);
+ write(0x06,1);
+ write(0x01,1);
+ // add functions
+ return {
+   write : write,
+   // clear screen
+   clear : function() { write(0x01,1); },
+   // print text
+   print : function(str) {
+     for (var i=0;i<str.length;i++)
+       write(Integer.valueOf(str.charAt(i)));
+   },
+   // flashing block for the current cursor, or underline
+   cursor : function(block) { write(block?0x0F:0x0E,1); },
+   // set cursor pos, top left = 0,0
+   setCursor : function(x,y) { var l=[0x00,0x40,0x14,0x54];write(0x80|(l[y]+x),1); },
+   // set special character 0..7, data is an array(8) of bytes, and then return to home addr
+   createChar : function(ch, data) {
+     write(0x40 | ((ch&7) << 3), 1);
+     for (var i=0; i<8; i++) write(data[i]);
+     write(0x80,1);
+   }
+ };
+}
+
+function connectI2C(/*=I2C*/_i2c) {
+ var i2c = _i2c;
+ var write = function(x, c) {
+  var a = (x&0xF0) |8| ((c===undefined)?1:0);
+  var b = ((x<<4)&0xF0) |8| ((c===undefined)?1:0);
+  i2c.writeTo(0x27, [a,a,a|4,a|4,a,a,b,b,b|4,b|4,b,b]);
+ };
+ return new HD44780(write);
+}
+
+function connect(/*=PIN*/_rs,/*=PIN*/_en,/*=PIN*/_d4,/*=PIN*/_d5,/*=PIN*/_d6,/*=PIN*/_d7) {
+ var data = [_d7,_d6,_d5,_d4];
+ var rs = _rs;
+ var en = _en;
+ digitalWrite([rs,en], 0);
+ var write = function(x, c) {
+  digitalWrite(rs, c===undefined);
+  digitalWrite(data, x>>4);
+  digitalPulse(en, 1, 0.01);
+  digitalWrite(data, x);
+  digitalPulse(en, 1, 0.01);
+ };
+ return new HD44780(write);
+}
+
---
+++ devices/big_number.js
@@ -0,0 +1,48 @@
+/* Copyright (c) 2013 Gordon Williams, Pur3 Ltd. See the file LICENSE for copying permission. */
+/* 
+Big Number library for use with 20x4 text-based LCDs. This fills the screen with 5 huge numbers of your choosing.
+
+```
+var lcd = require("HD44780").connect(A4,A5,A0,A1,A2,A3);
+var disp = require("big_number").use(lcd);
+disp.showNumber(12345);
+```  
+
+*/
+
+exports.use = function(lcd) {
+  var bigChars = 
+["415 431151153 3311411115415415",
+ "3 3  3223223323315315  3323723",
+ "3 3  33    3  3  33 3  33 3  3",
+ "726  3322226  3226726  3726226"];
+  // create the chars
+  lcd.createChar(1,[31,31,31,31,0,0,0,0]);
+  lcd.createChar(2,[0,0,0,0,0,31,31,31,31]);
+  lcd.createChar(3,[31,31,31,31,31,31,31,31]);
+  lcd.createChar(4,[1,3,7,15,31,31,31,31]);
+  lcd.createChar(5,[16,24,28,30,31,31,31,31]);
+  lcd.createChar(6,[31,31,31,31,30,28,24,16]);
+  lcd.createChar(7,[31,31,31,31,15,7,3,1]);
+  return {
+    // write a big number num at column number x
+    showDigit : function(x, num) {
+      for (var y=0;y<4;y++) {
+        this.setCursor(x,y);
+        for (var n=0;n<3;n++) {
+          var c = this.bigChars[y].charAt(num*3+n);
+          this.write(c==" "?32:parseInt(c,10));
+        }
+      }
+    },
+    // write the given 5 digit decimal on the LCD screen
+    showNumber : function(num) {
+      lcd.clear(); showDigit(17,num%10);
+      if (num>9) showDigit(14,(num/10)%10);
+      if (num>99) showDigit(11,(num/100)%10);
+      if (num>999) showDigit(8,(num/1000)%10);
+      if (num>9999) showDigit(5,(num/10000)%10);
+	}
+  };
+};
+
