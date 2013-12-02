608f12acae69a93a5c14979449f3a37151a78804
Gordon Williams <gw@pur3.co.uk>
2013-12-02 13:27:44 +0000

    updated docs and lib for LED driver

--- devices/MAX7219.js
+++ devices/MAX7219.js
@@ -3,30 +3,52 @@
 Module for the MAX7219 7 segment display driver
 
 ```
-SPI2.setup();
-var disp = require("MAX7219").connect(SPI2,B6);
-disp.set(12345);
-setTimeout(function() { disp.off(); }, 10000);
+SPI2.setup({mosi:B15, sck:B13});
+var disp = require("MAX7219").connect(SPI2,B14);
+disp.set("--HELP--"); // disp can display strings with the following chars: 0123456789-EHLP
+
+setTimeout(function() {
+  disp.raw([1,2,4,8,16,32,64,128]); // you can set the LEDs directly
+}, 1000);
+
+setTimeout(function() {
+  var n = 0;
+  setInterval(function() {
+    disp.set(n++); // it can display integers
+    disp.intensity(0.5+0.5*Math.sin(n*0.2)); // you set set intensity
+  }, 100);
+}, 2000);
 ```
 */
 exports.connect = function(/*=SPI*/_spi,/*=PIN*/_cs) {
-  var spi = _spi; 
+  var spi = _spi;
   var cs = _cs;
-  spi.send ([0x9,0xff],B6); // decode mode
-  spi.send ([0xa,0x4],B6); // scan limit
-  spi.send ([0xb,0x8],B6); // intensity
-  spi.send ([0xc,0x1],B6); // shutdown
-
+    spi.send ([0xA,0xF],cs);  // intensity  -full
+  spi.send ([0xB,0x7],cs);  // scan limit - all 8 chars
+  spi.send ([0xC,0],cs);    // shutdown
   return {
+   /// Display the given characters - only 0123456789-EHLP are possible
    set : function(val) {
-     for (var i=5;i>0;i--) {
-       spi.send([i,(val / 10000) % 10],cs);
-       print((val / 10000) % 10);
-       val *= 10;
+     spi.send ([0x9,0xFF],cs); // decode all as numbers
+     var map = "0123456789-EHLP  "; // FIXME indexOf doesn't find last index 
+     var s = "        "+val;
+     if (s.length>8) s = s.substr(s.length-8);
+     for (var i=0;i<8;i++) {
+       spi.send([8-i,map.indexOf(s[i])],cs);
      }
+     spi.send([0x0C,1],cs); // no shutdown
    },
-   off : function() {
-     spi.send([0x5,0xff,0x4,0xff,0x3,0xff,0x2,0xff,0x1,0xff],cs);
-   }
+   // Send the given raw LED data to the display
+   raw : function(val) {
+     spi.send ([0x9,0],cs); // output raw data
+     for (var i=0;i<val.length;i++) {
+       spi.send([i+1,val[i]],cs);
+     }
+     spi.send([0x0C,1],cs); // no shutdown
+   },
+   // Turn display off
+   off : function() { spi.send([0xC,0],cs); },
+   // Set intensity (0 to 1)
+   intensity : function(i) { spi.send([0xA,Math.clip(i*15,0,15)],cs); }
   };
-}
+};
