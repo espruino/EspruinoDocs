be74b14a0b54d33c815579eb01da57bdedbf0e84
Gordon Williams <gw@pur3.co.uk>
2014-01-09 10:04:32 +0000

    include videos via iframe

--- bin/build.js
+++ bin/build.js
@@ -192,7 +192,9 @@ markdownFiles.forEach(function (file) {
    contents = handleImages(file, contents);
    
    // replace simple links
-   contents = contents.replace(/\[\[http:\/\/youtu.be\/([a-zA-Z0-9_ ]+)\]\]/g,"[![Video Thumbnail](http://img.youtube.com/vi/$1/0.jpg)](http://www.youtube.com/watch?v=$1)"); // youtube
+//   contents = contents.replace(/\[\[http:\/\/youtu.be\/([a-zA-Z0-9_ ]+)\]\]/g,"[![Video Thumbnail](http://img.youtube.com/vi/$1/0.jpg)](http://www.youtube.com/watch?v=$1)"); // youtube
+   contents = contents.replace(/\[\[http:\/\/youtu.be\/([a-zA-Z0-9_ ]+)\]\]/g,
+           '<iframe allowfullscreen="" frameborder="0" height="360" src="http://www.youtube.com/embed/$1" width="640"></iframe>'); // youtube
    contents = contents.replace(/\[\[([a-zA-Z0-9_ ]+).js\]\]/g,"[$1](/modules/$1.js) ([About Modules](/Modules))");
    contents = contents.replace(/\[\[([a-zA-Z0-9_ ]+)\]\]/g,"[$1](/$1)");
    contents = contents.replace(/(\[.+\]\([^) ]+) ([^) ]+\))/g,"$1+$2");
