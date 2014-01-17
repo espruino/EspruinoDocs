ce24690cce711dffcc645c6d362ea99851480220
Gordon Williams <gw@pur3.co.uk>
2014-01-17 08:29:43 +0000

    intelligent minify

--- build.sh
+++ build.sh
@@ -19,27 +19,37 @@ cp html/refimages/* $WEBSITE/www/refimages
 
 MODULEDIR=$WEBSITE/www/modules
 mkdir -p $MODULEDIR
-rm $MODULEDIR/*
-cp devices/*.js $MODULEDIR
+
+#exit 0
 
 # Minify all modules
-for module in `ls $MODULEDIR/*.js`; do
+for module in `ls devices/*.js`; do
   BNAME=`basename $module .js`
   MINJS=${BNAME}.min.js
-  echo Minifying $MODULEDIR/$module to $MINJS
-  
-  # Minify, and convert binary to a proper number
-  sed "s/0b\([01][01]*\)/parseInt(\"\1\",2)/g" $module | curl -s \
-    -d compilation_level=SIMPLE_OPTIMIZATIONS \
-    -d output_format=text \
-    -d output_info=compiled_code \
-    --data-urlencode "js_code@/dev/fd/0" \
-   http://closure-compiler.appspot.com/compile > $MODULEDIR/$MINJS
+  # if file doesn't exist, write an empty file so diff works
+  if [ ! -f $MODULEDIR/$BNAME.js ]; then
+    echo > $MODULEDIR/$BNAME.js
+  fi
 
-   if [[ -s $MODULEDIR/$MINJS ]] ; then 
-     echo "$MODULEDIR/$MINJS compile successful"
-   else
-     echo "$MODULEDIR/$MINJS compile FAILED."
-     exit 1
-   fi ;
+  if diff devices/$BNAME.js $MODULEDIR/$BNAME.js >/dev/null ; then
+    echo "Module $BNAME hasn't changed, leaving"
+  else
+    echo "Module $BNAME is different or doesn't exist"  
+    cp devices/$BNAME.js $MODULEDIR/$BNAME.js
+    echo Minifying $MODULEDIR/$module to $MINJS  
+    # Minify, and convert binary to a proper number
+    sed "s/0b\([01][01]*\)/parseInt(\"\1\",2)/g" $module | curl -s \
+      -d compilation_level=SIMPLE_OPTIMIZATIONS \
+      -d output_format=text \
+      -d output_info=compiled_code \
+      --data-urlencode "js_code@/dev/fd/0" \
+     http://closure-compiler.appspot.com/compile > $MODULEDIR/$MINJS
+  
+     if [[ -s $MODULEDIR/$MINJS ]] ; then 
+       echo "$MODULEDIR/$MINJS compile successful"
+     else
+       echo "$MODULEDIR/$MINJS compile FAILED."
+       exit 1
+     fi 
+  fi
 done
