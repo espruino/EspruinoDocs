ecbde8297c5fd5b45fc6ec0207e305b2c696c63e
Gordon Williams <gw@pur3.co.uk>
2014-04-10 16:07:36 +0100

    Added Pixy module

---
+++ devices/Pixy.js
@@ -0,0 +1,114 @@
+/* Copyright (c) 2014 Gordon Williams, Pur3 Ltd. See the file LICENSE for copying permission. */
+/*
+Module for CMUcam5 'Pixy' camera
+
+
+```
+SPI2.setup({sck : B13, miso : B14, mosi : B15, baud : 1000000});
+var pixy = exports.connect(SPI2);
+console.log(pixy.getBlocks());
+```
+
+*/
+var C = {
+  PIXY_START_WORD        : 0xaa55,
+  PIXY_START_WORDX       : 0x55aa,
+  PIXY_MAXIMUM_ARRAYSIZE : 130,
+  PIXY_SYNC_BYTE         : 0x5a,
+  PIXY_SYNC_BYTE_DATA    : 0x5b,
+  PIXY_OUTBUF_SIZE       : 6
+};
+
+function Pixy(spi) {
+  this.spi = spi;
+  this.skipStart = false;
+  this.outIndex = 0;
+  this.outBuf = undefined;
+}
+  
+Pixy.prototype.getWord = function() {
+  // ordering is different because Pixy is sending 16 bits through SPI 
+  // instead of 2 bytes in a 16-bit word as with I2C
+  var w, cout = 0;
+
+  if (outBuf)
+  {
+    w = this.spi.send(C.PIXY_SYNC_BYTE_DATA);
+    cout = outBuf[outIndex++];
+    if (outIndex==outLen)
+      outBuf = undefined; 
+  }
+  else
+    w = this.spi.send(C.PIXY_SYNC_BYTE);
+  return (w<<8) | this.spi.send(cout);
+};
+  
+Pixy.prototype.getByte = function() {
+  return this.spi.send(0x00);
+};
+Pixy.prototype.send = function(data) {
+  if (outBuf) return -1;
+  outBuf = data;
+  outIndex = 0;
+  return len;
+};
+
+Pixy.prototype.getStart = function() {
+  var lastw = 0xffff;
+  while(true) {
+    var w = this.getWord();
+
+    if (w===0 && lastw===0) {
+//    delayMicroseconds(10);
+      return false;
+    } else if (w==C.PIXY_START_WORD && lastw==C.PIXY_START_WORD)
+      return true;
+    else if (w==C.PIXY_START_WORDX) {
+      console.log("reorder");
+      this.getByte(); // resync
+    }
+    lastw = w;
+  }
+};
+
+Pixy.prototype.getBlocks = function() {
+  var blocks = [];
+  
+  if (!this.skipStart) {
+    if (!this.getStart())
+      return [];
+  } else
+    this.skipStart = false;
+	
+  while (blocks.length < C.PIXY_MAXIMUM_ARRAYSIZE) {
+    var checksum = this.getWord();
+    if (checksum==C.PIXY_START_WORD) {
+      // we've reached the beginning of the next frame
+      this.skipStart = true;
+      return blocks;
+    }
+    else if (checksum===0)
+      return blocks;
+
+    var block = {
+      id : this.getWord(),
+      x : this.getWord(),
+      y : this.getWord(),
+      width : this.getWord(),
+      height : this.getWord()
+    };
+
+    if (checksum==block.id+block.x+block.y+block.width+block.height)
+      blocks.push(block);
+    else
+      console.log("cs error");
+
+    if (this.getWord()!=C.PIXY_START_WORD)
+      return blocks;
+  }
+  return blocks;
+};
+  
+exports.connect = function(spi) {
+  return new Pixy(spi);
+};
---
+++ devices/Pixy.md
@@ -0,0 +1,64 @@
+<!--- Copyright (c) 2014 Gordon Williams. See the file LICENSE for copying permission. -->
+CMUcam5 Pixy
+============
+
+* KEYWORDS: Camera,CMUcam5,Pixy,Computer Vision
+
+![CMUcam5 Pixy](module.jpg)
+
+The Pixy is a computer vision camera that does all the hard work on-board. It can recognize coloured objects that it 'sees', and will then report their size and position back to a connected microcontroller.
+
+The official documentation is here: [http://cmucam.org/projects/cmucam5/wiki/Quick_Start]
+
+There's an Espruino module called [[Pixy.js]] which will interface to it.
+
+Wiring Up
+---------
+
+![Pixy Pins](pins.jpg)
+
+Use SPI (the default for Pixy), and connect as follows:
+
+| CMUCam |  |Espruino |
+|--------|--|--------|
+| 1 | SPI MISO | B14 |
+| 2 | 5V | Bat |
+| 3 | SPI SCK | B13 |
+| 4 | SPI MOSI | B15 |
+| 5 | I2C SCL | |
+| 6 | GND | GND |
+| 7 | SPI SS |  |
+| 8 | GND |  |
+| 9 | I2C SDA | |
+| 10 | GND | |
+
+**Note:** Pins 7 to 10 are not exposed on the ribbon cable that comes with the pixy.
+
+Software
+-------
+
+```
+SPI2.setup({sck : B13, miso : B14, mosi : B15, baud : 1000000});
+var pixy = require("Pixy").connect(SPI2);
+console.log(pixy.getBlocks());
+```
+
+returns something like:
+
+```JSON
+[
+  {"id":1,"x":18,"y":24,"width":17,"height":2}
+]
+```
+
+**Note:** There may currently be issues with the minified version of this module. To use the unminified version, change `.min.js|.js` to `.js|.min.js` in the Web IDE's settings menu.
+
+Using 
+-----
+
+* APPEND_USES: Pixy
+
+Buying
+-----
+
+* [Order from Charmed Labs](http://charmedlabs.com/)
