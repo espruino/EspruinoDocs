7ee28acb83075113cb51827244fcb234f80cd40b
Luca de Marinis <loop23@gmail.com>
2014-04-12 17:03:50 +0200

    vars instead of this

--- devices/Midi.js
+++ devices/Midi.js
@@ -1,41 +1,40 @@
 function Midi(uart, speed, noteOn) {
-	this.midi = uart,
-	this.midi.setup(speed, { parity:'none', bytesize:8, stopbits:1 });
+	var midi = uart;
+	midi.setup(speed, { parity:'none', bytesize:8, stopbits:1 });
 
-	console.log("Port: " + this.midi + ' initialized');
-	this.state = 'WAITING';
-	this.message = '';
-	this.p1 = 0;
-	this.p2 = 0;
-	this.chan = 0;
-	this.noteOn = noteOn;
+	console.log("Port: " + midi + ' initialized');
+	var state = 'WAITING';
+	var message = '';
+	var p1 = 0;
+	var p2 = 0;
+	var channel = 0;
 
-	this.midi.onData(function(d) {
+	midi.onData(function(d) {
 		var data = d.data;
 		if (data.length != 1) {
 	    console.log("Weird multibyte thing, discarding");
 		}
 		var b = data.charCodeAt(0);
     console.log("Got data!" + b + ' - I am: ' + this);
-		if (this.state == 'WAITING') {
+		if (state == 'WAITING') {
 	    if (b < 0x80) {
 				print("Out of order or non-command, discarding");
 	    } else { // command
-				this.channel = b & 0b1111;
-				this.message = b & 0b11110000;
-				console.log("Command: " + this.message + ' on channel: ' + this.channel);
-				this.state = 'P1';
+				channel = b & 0b1111;
+				message = b & 0b11110000;
+				console.log("Command: " + message + ' on channel: ' + channel);
+				state = 'P1';
 	    }
-		} else if (this.state == 'P1') {
-			this.p1 = b;
-	    this.state = 'P2';
-		} else if (this.state == 'P2') {
-			this.p2 = b;
+		} else if (state == 'P1') {
+			p1 = b;
+	    state = 'P2';
+		} else if (state == 'P2') {
+			p2 = b;
 	    // print("So, mess: " + this.message + ' ,p1: ' + this.p1 + ' ,p2: ' + this.p2);
-			if (this.message == 0x90) {
-				this.noteOn(this.channel, this.p1, this.p2);
+			if (message == 0x90) {
+				noteOn(channel, p1, p2);
 			}
-			this.state = 'WAITING';
+			state = 'WAITING';
 		}
 	});
 };
