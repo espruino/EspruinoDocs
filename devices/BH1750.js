3749fc025949b2787f3d332eb6601f50bb7068cc
Gordon Williams <gw@pur3.co.uk>
2014-11-20 09:47:51 +0000

    tweaks

--- devices/BH1750.js
+++ devices/BH1750.js
@@ -1,5 +1,5 @@
-  /* Copyright (C) 2014 Spence Konde. See the file LICENSE for copying permission. */
-  /*
+/* Copyright (C) 2014 Spence Konde. See the file LICENSE for copying permission. */
+/*
 This module interfaces with the Rohm's BH1750 I2C Light Sensor. 
 
 Usage is very simple. Setup I2C, and call
@@ -26,16 +26,15 @@ console.log(bh.read());
 
 Note that it takes up to 120ms to complete a measurement at high resolution, so if doing one-time measurements, use setTimeout() to call bh.read() after waiting.
 
-
 */
 
 
 exports.connect = function(i2c, addr) {
-	if (addr) {
-		return new BH1750(i2c, 0x5C);
-	} else {
-	    return new BH1750(i2c, 0x23);
-	}
+  if (addr) {
+    return new BH1750(i2c, 0x5C);
+  } else {
+    return new BH1750(i2c, 0x23);
+  }
 };
 
 function BH1750(i2c, addr) {
@@ -59,31 +58,31 @@ BH1750.prototype.power= function(onoff) {
 };
 
 BH1750.prototype.read= function() {
-	if (this.active) {
-		var data=this.i2c.readFrom(this.i2ca,2);
-		data=(data[0]<<8)+data[1];
-		if (this.onetime) {
-			this.active=0;
-		}
-		if (this.active==3) {
-			return (data*0.5)/this.factor;
-		} else {
-			return data/this.factor;
-		}
-	} else {
-		throw "Device not active"
-	}
+  if (this.active) {
+    var data=this.i2c.readFrom(this.i2ca,2);
+    data=(data[0]<<8)+data[1];
+    if (this.onetime) {
+      this.active=0;
+    }
+    if (this.active==3) {
+      return (data*0.5)/this.factor;
+    } else {
+      return data/this.factor;
+    }
+  } else {
+    throw "Device not active"
+  }
 };
 
 BH1750.prototype.start= function(resolution,onetime) { //resolution: 1 = 1 lx (recomended by mfg), 2 = 4 lx, 3 = 0.5 lx
-	this.i2c.writeTo(this.i2ca,(onetime?32:16)+(resolution==1?0:(resolution==2?3:1)));
-	this.active=resolution;
-	this.onetime=onetime;
+  this.i2c.writeTo(this.i2ca,(onetime?32:16)+(resolution==1?0:(resolution==2?3:1)));
+  this.active=resolution;
+  this.onetime=onetime;
 };
 
 BH1750.prototype.setMT=function(mt) {
-	this.factor=1.2/(69/X);
-	mt=E.clip(mt,31,254);
-	this.i2c.writeTo(this.i2ca,0x80+((mt&0xE0)>>3));
-	this.i2c.writeTo(this.i2ca,0xB0+(mt&0x1F));
+  this.factor=1.2/(69/X);
+  mt=E.clip(mt,31,254);
+  this.i2c.writeTo(this.i2ca,0x80+((mt&0xE0)>>3));
+  this.i2c.writeTo(this.i2ca,0xB0+(mt&0x1F));
 }
