a211ada4079ac716cbd8cf6fdfdda25e9228f04c
Spence Konde <spencekonde@gmail.com>
2015-08-17 12:21:49 -0400

    Update MCP23008.js

--- devices/MCP23008.js
+++ devices/MCP23008.js
@@ -22,14 +22,13 @@ function MCP23008(i2c,rst, i2ca) {
 }
 MCP23008.prototype.s=function(r,d){this.­i2c.writeTo(this.i2ca,r,d);};
 MCP23008.prototype.r=function(r){this.i2­c.writeTo(this.i2ca,r);return this.i2c.readFrom(this.i2ca,1)};
-MCP23008.prototype.mode=function(pin,mode) {
-  if (["input","output","input_pullup"].index­Of(mode)<0) throw "Pin mode "+mode+" not available";
-  var bv=1<<pin;
+MCP23008.prototype.mode=function(pin,mode) {this.m(1<<pin,mode);};
+
+MCP23008.prototype.m=function(bv,mode) {
+  if (["input","output","input_pullup"].indexOf(mode)<0) throw "Pin mode "+mode+" not available";
   this.s(0,mode=='output'?(this.m&=~bv):(this.m |=bv));
   this.s(6,mode=='input_pullup'?(this.pu|=bv):(this.pu&=~bv));
 };
-
-MCP32008.prototype.mode=function(pin,mode) {
 
 MCP23008.prototype.write=function(pin,val) {
     var bv=1<<pin;
@@ -48,4 +47,4 @@ PEP.prototype.set=function(){this.p.s(9,this.p.olat|=this.b)};
 PEP.prototype.reset=function(){this.p.s(9,this.p.olat&=~(this.b))};
 PEP.prototype.write=function(v){this.p.s(9,(v?this.p.olat|=this.b:this.p.olat&=~(this.b)))};
 PEP.prototype.read=function(){return (this.p.r(9)[0]&this.b)?1:0};
-PEP.prototype.mode=function(m){this.p.mode(this.b,m)};
+PEP.prototype.mode=function(m){this.p.m(this.b,m)};
