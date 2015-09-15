648683543e86a3c78b23b485b8421a6e5bf7a0d6
Spence Konde <spencekonde@gmail.com>
2015-09-15 12:11:22 -0400

    Fix issue with two definitions of m

--- devices/MCP23008.js
+++ devices/MCP23008.js
@@ -1,16 +1,19 @@
+
 exports.connect = function(i2c,rst,i2ca) {
     
     return new MCP23008(i2c,rst,i2ca);
 };
 function MCP23008(i2c,rst, i2ca) {
-  rst.write(0);
+  if (rst) {
+    rst.write(0);
+  }
   this.i2c = i2c;
   this.i2ca = (i2ca===undefined) ? 32:32+i2ca;
   if (rst) {
       this.rst=rst;
       this.rst.write(1);
   }
-  this.m=255;
+  this.n=255;
   this.pu=0;
   this.olat=0;
   this.A0=new PEP(1,this);
@@ -23,12 +26,10 @@ function MCP23008(i2c,rst, i2ca) {
   this.A7=new PEP(128,this);
 }
 MCP23008.prototype.s=function(r,d){this.i2c.writeTo(this.i2ca,r,d);};
-MCP23008.prototype.r=function(r){this.i2c.writeTo(this.i2ca,r);return this.i2c.readFrom(this.i2ca,1)};
-MCP23008.prototype.mode=function(pin,mode) {this.m(1<<pin,mode);};
-
+MCP23008.prototype.r=function(r){this.i2c.writeTo(this.i2ca,r);return this.i2c.readFrom(this.i2ca,1);};
 MCP23008.prototype.m=function(bv,mode) {
   if (["input","output","input_pullup"].indexOf(mode)<0) throw "Pin mode "+mode+" not available";
-  this.s(0,mode=='output'?(this.m&=~bv):(this.m |=bv));
+  this.s(0,mode=='output'?(this.n&=~bv):(this.n |=bv));
   this.s(6,mode=='input_pullup'?(this.pu|=bv):(this.pu&=~bv));
 };
 
@@ -42,22 +43,18 @@ MCP23008.prototype.writePort=function(val) {
     this.s(9,this.olat);
 }; 
 MCP23008.prototype.read=function(pin) {
-    var bv=1<<pin;
-    return (this.r(9)[0]&bv)>>pin;
+    return (this.r(9)[0]&(1<<pin))>>pin;
 }; 
 MCP23008.prototype.readPort=function() {
     return (this.r(9)[0]);
 }; 
-MCP23008.prototype.read=function(pin) {
-    var bv=1<<pin;
-    return (this.r(9)[0]&bv)>>pin;
-}; 
+MCP23008.prototype.mode=function(pin,mode) {this.m(1<<pin,mode);};
 function PEP (b,p){
     this.b=b;
     this.p=p;
 }
-PEP.prototype.set=function(){this.p.s(9,this.p.olat|=this.b)};
-PEP.prototype.reset=function(){this.p.s(9,this.p.olat&=~(this.b))};
-PEP.prototype.write=function(v){this.p.s(9,(v?this.p.olat|=this.b:this.p.olat&=~(this.b)))};
-PEP.prototype.read=function(){return (this.p.r(9)[0]&this.b)?1:0};
-PEP.prototype.mode=function(m){this.p.m(this.b,m)};
+PEP.prototype.set=function(){this.p.s(9,this.p.olat|=this.b);};
+PEP.prototype.reset=function(){this.p.s(9,this.p.olat&=~(this.b));};
+PEP.prototype.write=function(v){this.p.s(9,(v?this.p.olat|=this.b:this.p.olat&=~(this.b)));};
+PEP.prototype.read=function(){return (this.p.r(9)[0]&this.b)?1:0;};
+PEP.prototype.mode=function(m){this.p.m(this.b,m);};
