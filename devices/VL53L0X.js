a0727888b8b2aa1ae2a5fadb32f5d694b488255d
Joachim Klein <j.k_klein@web.de>
2017-07-23 20:19:59 +0200

    Add changeAddress
    This change add the change Address funktion. This allows to use more than one sensor on the same I2C Interface. The implementation is fully transparent to the previous version. No change for old code is required!

--- devices/VL53L0X.js
+++ devices/VL53L0X.js
@@ -4,11 +4,19 @@ var C = {
   REG_RESULT_RANGE_STATUS : 0x0014
 };
 
+/** 
+Init VL53 Sensor with default address.
+Store Address in a variable, this allows to change the address later.
+*/
 function VL53L0X(i2c, options) {
   this.options = options||{};
   this.i2c = i2c;
-  //this.r(0xC0,1)[0]==0xEE;
-  // initialise
+  this.ad = 0x52>>1;
+  this.init();
+}
+
+/** initialise VL53L0X */
+VL53L0X.prototype.init = function() {
   this.w(0x80, 0x01);
   this.w(0xFF, 0x01);
   this.w(0x00, 0x00);
@@ -16,14 +24,20 @@ function VL53L0X(i2c, options) {
   this.w(0x00, 0x01);
   this.w(0xFF, 0x00);
   this.w(0x80, 0x00);
-}
+};
+
+/** this function set a new device address.
+Call this function before you initialise the VL53! */
+VL53L0X.prototype.a = function(addr) {
+  this.i2c.writeTo(0x52>>1, 0x8a, addr);
+};
 
 VL53L0X.prototype.r = function(addr,n) {
-  this.i2c.writeTo(0x52>>1, addr);
-  return this.i2c.readFrom(0x52>>1, n);
+  this.i2c.writeTo(this.ad, addr);
+  return this.i2c.readFrom(this.ad, n);
 };
 VL53L0X.prototype.w = function(addr,d) {
-  this.i2c.writeTo(0x52>>1, addr, d);
+  this.i2c.writeTo(this.ad, addr, d);
 };
 
 /** Perform one measurement and return the result.
@@ -61,6 +75,16 @@ VL53L0X.prototype.performSingleMeasurement = function() {
   // TODO: use LinearityCorrectiveGain/etc
   return res;
 };
+
+/** Parameters
+   newAddress -> new I2C Address which should be used for this device
+				 The Address should be a multiple of 2! Please consider
+				 other devices on the bus!
+ */
+VL53L0X.prototype.changeAddress = function( newAddress ) {
+	this.a( newAddress>>1 );
+	this.ad = newAddress>>1;
+}
 
 exports.connect = function(i2c, options) {
   return new VL53L0X(i2c, options);
