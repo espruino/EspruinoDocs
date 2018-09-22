a74a2a32cabf12933ca6d83f2d73e28abe14674b
sleuthware <sleuthware@gmail.com>
2018-09-22 12:23:50 -0500

    Add files via upload

--- modules/moduleNeopixelColor.js
+++ modules/moduleNeopixelColor.js
@@ -4,6 +4,13 @@
 //https://www.w3schools.com/cssref/css_colors.asp
 
 
+
+
+//var exports={};
+
+
+
+
 class Color {
 
   constructor(obj) {
@@ -12,13 +19,32 @@ class Color {
     if (typeof obj == "string") {
       
       // See if is a name or a specified RGB or hex value
+      var isHex = this.isHexColor(obj);
+if( isHex ) {
+                  this.colorName = "notassigned";
+            this.colorRGB = obj;
+  
+           console.log( "Color: obj " + obj );
+
+            this.colorR = obj.substring(0, 2);
+            this.colorG = obj.substring(2, 4);
+            this.colorB = obj.substring(4, 6);
+
+
+      /*
       var cvrt = this.cvrtHexStToHexN(obj);
       if (typeof cvrt == "number") {
         
+        
+           console.log( "Color: cvrt " + cvrt );
+         
+        
             this.colorName = "notassigned";
-            this.colorRGB = cvrt.substring(2, 8);
+//            this.colorRGB = cvrt.substring(2, 8);
       }
-
+*/
+      
+      
 
 
     } else {
@@ -27,6 +53,7 @@ class Color {
       
       var objRGB = JSON.parse(JSON.stringify(RGB));
       var key;
+console.log( "Color: objRGB " + objRGB );
 
       for (key in objRGB) {
         if (objRGB.hasOwnProperty(key)) {
@@ -38,13 +65,15 @@ class Color {
               return (NaN);
             }
 
+console.log( "Color: match key " + key );
+
             this.colorName = key;
             this.colorRGB = objRGB[key];
-/*
+
             this.colorR = objRGB[key].substring(0, 2);
             this.colorG = objRGB[key].substring(2, 4);
             this.colorB = objRGB[key].substring(4, 6);
-
+/*
             this.decR = this.cvrtHexToDec(this.colorR);
             this.decG = this.cvrtHexToDec(this.colorG);
             this.decB = this.cvrtHexToDec(this.colorB);
@@ -63,7 +92,7 @@ class Color {
 
           }
 
-
+        //if (objRGB.hasOwnProperty(key))
         }
       //for (key in objRGB)  
       }
@@ -72,9 +101,14 @@ class Color {
 
    
     } // else
-            this.colorR = objRGB[key].substring(0, 2);
-            this.colorG = objRGB[key].substring(2, 4);
-            this.colorB = objRGB[key].substring(4, 6);
+//            this.colorR = objRGB[key].substring(0, 2);
+//            this.colorG = objRGB[key].substring(2, 4);
+//            this.colorB = objRGB[key].substring(4, 6);
+
+    //if (typeof obj == "string")
+    }
+    
+    console.log( "Color: match aft else" );
 
             this.decR = this.cvrtHexToDec(this.colorR);
             this.decG = this.cvrtHexToDec(this.colorG);
@@ -99,6 +133,7 @@ class Color {
 
 
     return (this.colorObjJson);
+  //constructor(obj)    
   }
 
   setColorA(colorValDec) {
@@ -180,6 +215,46 @@ class Color {
     return this.cvrtHexToDec(shex);
   }
 
+  
+//  https://stackoverflow.com/questions/8027423/how-to-check-if-a-string-is-a-valid-hex-color-representation
+  isHexColor(val){
+  return( (typeof val == "string") && (val.length == 6)
+         && !isNaN( parseInt(val, 16) ) );
+  }
+  
 
 }
 exports = Color;
+
+
+
+
+
+
+/*
+var exports={};
+
+
+exports.Color;
+
+
+
+var RGB = require("https://github.com/sleuthware/EspruinoDocs/blob/master/modules/moduleNeopixelColors.js");
+*/
+
+
+
+
+/*
+
+// Create a copy of our constants array
+var RGB = require("https://github.com/sleuthware/EspruinoDocs/blob/master/modules/moduleNeopixelColors.js");
+
+var col = new Color("Aqua");
+
+var col = new Color("00ffab");
+
+*/
+
+
+
