d49bc6ac1cf662378501f5e9ea296c31fa5837de
sleuthware <sleuthware@gmail.com>
2018-09-23 04:31:19 -0500

    Add files via upload

---
+++ modules/moduleNeopixelPalettes.js
@@ -0,0 +1,28 @@
+
+
+
+
+//https://www.w3schools.com/cssref/css_colors.asp
+
+
+var PALETTES = {
+  Rainbow    : [
+      RGB.Red,
+      RGB.Orange,
+      RGB.Yellow,
+      RGB.Green,
+      RGB.Aqua,
+      RGB.Blue,
+      RGB.Purple,
+      RGB.Pink ],
+
+  RedWhiteBlue : [
+      RGB.Red,
+      RGB.White,
+      RGB.Blue ]
+
+  
+  };
+
+exports = PALETTES;
+
