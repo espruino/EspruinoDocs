ffa29f06ae41c1f9650badf358e15f50e5309e64
sleuthware <sleuthware@gmail.com>
2018-09-23 10:02:03 -0500

    Add files via upload

---
+++ modules/moduleConstantsTest.js
@@ -0,0 +1,47 @@
+//moduleConstantsTest.js
+//Sun 2018.09.23
+
+//http://www.espruino.com/Writing+Modules
+
+
+
+var C = {
+  U : "up",
+  D : "down",
+  L : "left",
+  R : "right",
+  N : "north",
+  E : "east",
+  S : "south"
+  W : "west",
+  T : "top",
+  M : "middle",
+  B : "bottom"
+};
+  
+exports = C;
+
+
+
+
+
+var GROUPS = {
+  
+  COMPASS   : [{N:C.N}, {E:C.E}, {S:C.S}, {W:C.W}],
+//  COMPASS   : [ C.N, C.E, C.S, C.W ],  
+  DIRECTION : [ C.U, C.D, C.L, C.R ],
+  VERTICAL  : [ C.T, C.M, C.B ]
+  
+};
+
+exports = GROUPS;
+
+
+var exports={};
+
+
+
+
+
+
+//[eof]
