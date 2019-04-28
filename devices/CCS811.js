ccd2a2d7c367cf64912f58169a7cdb5010efbc60
Lukács Ákos <AkosLukacs42@gmail.com>
2019-04-28 22:51:01 +0200

    CCS811 improvements
    - support alternative I2C address
    - support different sample rates
    - use nWake to save power on lower sample rates
    - and some docs

--- devices/CCS811.js
+++ devices/CCS811.js
@@ -1,64 +1,94 @@
 /* Copyright (c) 2018 Gordon Williams, Pur3 Ltd. See the file LICENSE for copying permission. */
 
 var C = {
-  WHO_AM_I : 0x20,
-  WHO_AM_I_VALUE : 0x81,
-  
-  STATUS : 0x00,
-  STATUS_ERROR : 0x01,
-  STATUS_DATA_READY : 0x08,
-  STATUS_APP_VALID : 0x10,
-  STATUS_FW_MODE : 0x80,
-  
-  MEAS_MODE : 0x01,
-  MEAS_MODE_INT_THRESH : 0x04,
-  MEAS_MODE_INT_DATARDY : 0x08,
-  MEAS_MODE_DRIVE_MODE_IDLE : 0x00,
-	MEAS_MODE_DRIVE_MODE_1SEC : 0x10,
-	MEAS_MODE_DRIVE_MODE_10SEC : 0x20,
-	MEAS_MODE_DRIVE_MODE_60SEC : 0x30,
-	MEAS_MODE_DRIVE_MODE_250MS : 0x40,
-  
-  ALG_RESULT_DATA : 0x02,
-  
-  SW_RESET : 0xFF,
-  BOOTLOADER_APP_START : 0xF4,   
+  WHO_AM_I: 0x20,
+  WHO_AM_I_VALUE: 0x81,
+
+  STATUS: 0x00,
+  STATUS_ERROR: 0x01,
+  STATUS_DATA_READY: 0x08,
+  STATUS_APP_VALID: 0x10,
+  STATUS_FW_MODE: 0x80,
+
+  MEAS_MODE: 0x01,
+  MEAS_MODE_INT_THRESH: 0x04,
+  MEAS_MODE_INT_DATARDY: 0x08,
+  /** Mode 0 – Idle (Measurements are disabled in this mode) */
+  MEAS_MODE_DRIVE_MODE_IDLE: 0x00,
+  /** Mode 1 – Constant power mode, IAQ measurement every second */
+  MEAS_MODE_DRIVE_MODE_1SEC: 0x10,
+  /** Mode 2 – Pulse heating mode IAQ measurement every 10 seconds */
+  MEAS_MODE_DRIVE_MODE_10SEC: 0x20,
+  /** Mode 3 – Low power pulse heating mode IAQ measurement every 60 seconds */
+  MEAS_MODE_DRIVE_MODE_60SEC: 0x30,
+  /** Mode 4 – Constant power mode, sensor measurement every 250ms */
+  MEAS_MODE_DRIVE_MODE_250MS: 0x40,
+
+  ALG_RESULT_DATA: 0x02,
+  /** Environment Data register */
+  ENV_DATA: 0x05,
+  SW_RESET: 0xFF,
+  BOOTLOADER_APP_START: 0xF4,
 };
 
-/* Set up the CCS811.
-
-options = {
-  int : pin, // optional - DRDY interrupt pin. If specified, 'data' event with data from 'get' will be emitted when data is ready
+/** Get the value for the drive mode register. */
+function getDriveMode(mode) {
+  switch (mode) {
+    case 0: return C.MEAS_MODE_DRIVE_MODE_IDLE;
+    case 1: return C.MEAS_MODE_DRIVE_MODE_1SEC;
+    case 2: return C.MEAS_MODE_DRIVE_MODE_10SEC;
+    case 3: return C.MEAS_MODE_DRIVE_MODE_60SEC;
+    case 4: return C.MEAS_MODE_DRIVE_MODE_250MS;
+    default: throw "Invalid mode!" + mode;
+  }
 }
-*/
-function CCS811(r,w,options) {
-  this.r = r; // read from a register
-  this.w = w; // write to a register
+
+/** Helper function to set the nWake pin low / high before and after I2C communication.
+ * (used if nWake is present in the options) */
+function wrapWithnWake(nWakePin, i2cFunc) {
+  return function(arg1, arg2) {
+    nWakePin.write(0);
+    // According to the datasheet, you should wait 50us after setting nWake to low, but the Espruino interpreter is slow enough :)
+    var i2cResult = i2cFunc(arg1, arg2);
+    nWakePin.write(1);
+    return i2cResult;
+  }
+}
+
+/** Set up the CCS811.
+
+(see details at exports.connectI2C) */
+function CCS811(r, w, options) {
+
+  this.r = (options && options.nWake) ? wrapWithnWake(options.nWake, r) : r; // read from a register
+  this.w = (options && options.nWake) ? wrapWithnWake(options.nWake, w) : w; // write to a register
   this.options = options || {};
-  
+
   this.w(C.SW_RESET, [0x11, 0xE5, 0x72, 0x8A]); // software reset
   var ccs = this;
   setTimeout(function() {
     if (ccs.r(C.WHO_AM_I, 1)[0] != C.WHO_AM_I_VALUE)
-      throw "CCS811 WHO_AM_I check failed";  
+      throw "CCS811 WHO_AM_I check failed";
     // start bootloader
-    ccs.w(C.BOOTLOADER_APP_START,[]);
+    ccs.w(C.BOOTLOADER_APP_START, []);
     setTimeout(function() {
-      if (!ccs.r(C.STATUS_FW_MODE,1)[0]&C.STATUS_FW_MODE)
+      if (!ccs.r(C.STATUS_FW_MODE, 1)[0] & C.STATUS_FW_MODE)
         throw "CCS811 not in FW mode";
+
+      var driveMode = getDriveMode(ccs.options.mode || 1);
       if (ccs.options.int) {
         ccs.watch = setWatch(function() {
           ccs.emit('data', ccs.get());
-        }, ccs.options.int, {edge:"falling",repeat:true});
-        // DRDY IRQ, 1 sec drive
-        ccs.w(C.MEAS_MODE, C.MEAS_MODE_DRIVE_MODE_1SEC | C.MEAS_MODE_INT_DATARDY);
+        }, ccs.options.int, {edge: "falling", repeat: true});
+        // DRDY IRQ
+        ccs.w(C.MEAS_MODE, driveMode | C.MEAS_MODE_INT_DATARDY);
       } else {
-        // 1 sec drive
-        ccs.w(C.MEAS_MODE, C.MEAS_MODE_DRIVE_MODE_1SEC);
+        // no interrupt
+        ccs.w(C.MEAS_MODE, driveMode);
       }
-    },100);
-  },100);
-  
+    }, 100);
+  }, 100);
+
 }
 // Shut down the CCS811
 CCS811.prototype.stop = function() {
@@ -68,36 +98,45 @@ CCS811.prototype.stop = function() {
 };
 // Returns true if data is available
 CCS811.prototype.available = function() {
-  return (this.r(C.STATUS, 1)[0] & C.STATUS_DATA_READY)!=0;
+  return (this.r(C.STATUS, 1)[0] & C.STATUS_DATA_READY) != 0;
 };
 
 /* read the current environment settings, assuming available()==true.
-   {
-     eCO2 : int, // equivalent CO2, in ppm (400..8192)
-     TVOC : int, // Total Volatile Organic Compounds, in ppb (0..1187)
-     new : bool  // true if this is a new reading
-   }
-   ec02 and TVOC values are clipped to the given ranges - so for instance you'll never see a CO2 value below 400. 
+{
+ eCO2 : int, // equivalent CO2, in ppm (400..8192)
+ TVOC : int, // Total Volatile Organic Compounds, in ppb (0..1187)
+ new : bool  // true if this is a new reading
+}
+ec02 and TVOC values are clipped to the given ranges - so for instance you'll never see a CO2 value below 400.
 */
 CCS811.prototype.get = function() {
-  var isNew = (this.r(C.STATUS, 1)[0] & C.STATUS_DATA_READY)!=0;
-  var d = this.r(C.ALG_RESULT_DATA,  4); // could read 8, but don't need last 4
+  var isNew = (this.r(C.STATUS, 1)[0] & C.STATUS_DATA_READY) != 0;
+  var d = this.r(C.ALG_RESULT_DATA, 4); // could read 8, but don't need last 4
   /* NOTE: STATUS is 5th data element, but because we've just read
   ALG_RESULT_DATA, STATUS_DATA_READY is always 0! */
-	return { 
-    eCO2 : (d[0]<<8)|d[1],
-    TVOC : (d[2]<<8)|d[3],
-    new : isNew 
+  return {
+    eCO2: (d[0] << 8) | d[1],
+    TVOC: (d[2] << 8) | d[3],
+    new: isNew
   };
 };
 
-// Initialise the CCS811 module with the given I2C interface
-exports.connectI2C = function(i2c,options) {  
-  var addr = 0x5A;
-  return new CCS811(function(reg,len) { // read
-    i2c.writeTo(addr,reg);
-    return i2c.readFrom(addr,len);
-  }, function(reg,data) { // write
-    i2c.writeTo(addr,reg,data);
-  },options);
+/*
+Initialise the CCS811 module with the given I2C interface
+```
+options = {
+  int : pin // optional - DRDY interrupt pin. If specified, 'data' event with data from 'get' will be emitted when data is ready
+  addr: 0x5A | 0x5B // optional - I2C address of the CCS811, defaults to 0x5A. When ADDR pin of the CCS811 is low, the I2C address 0x5A. When ADDR is high the I2C address is 0x5B.
+  mode: 0..4 // optional - Operation mode, defaults to 1: Constant power mode, IAQ measurement every second
+  nWake: pin // optional - Connect a pin to the CCS811's nWake pin to the lower it's power consumption in Mode 2 or Mode 3
+}
+``` */
+exports.connectI2C = function(i2c, options) {
+  var addr = (options && options.addr) || 0x5A;
+  return new CCS811(function(reg, len) { // read
+    i2c.writeTo(addr, reg);
+    return i2c.readFrom(addr, len);
+  }, function(reg, data) { // write
+    i2c.writeTo(addr, reg, data);
+  }, options);
 };
