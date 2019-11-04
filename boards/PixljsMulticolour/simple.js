67a444f7570f56fbe41965787193030d1bf7237c
Mark Becker <mark.becker@sbit-gmbh.de>
2019-11-04 17:54:11 +0100

    fix Uncaught SyntaxError
    Unexpected end of Input  at line 1 col 22
    at line 1 col 22
    Badge.getName = () =>

--- boards/PixljsMulticolour/simple.js
+++ boards/PixljsMulticolour/simple.js
@@ -28,10 +28,7 @@ Badge.reset = () => {
 // Should the badge be connectable?
 Badge.connectable = false;
 // --------------------------------------------
-Badge.getName = () =>
-  NRF.getAddress()
-    .substr(-5)
-    .replace(":", "");
+Badge.getName = () => "Pixl.js " + NRF.getAddress().substr(-5).replace(":", "");
 Badge.updateBLE = () => {
   var adv = {
     showName: Badge.connectable,
