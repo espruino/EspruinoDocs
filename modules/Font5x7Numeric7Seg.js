494ab129968c5f5704bdc2e61637e6dae3549315
Gordon Williams <gw@pur3.co.uk>
2020-05-06 11:23:36 +0100

    Add  : to 7 seg fonts

--- modules/Font5x7Numeric7Seg.js
+++ modules/Font5x7Numeric7Seg.js
@@ -2,54 +2,63 @@
 /* 7 segment 5x7 font - only '.0123456789ABCDEF' */
 /*
 // Magic 7 segment font maker
- var W = 5; // width +1 for empty column
- var H = 7;
- var base = `
-  aa
- f  b
- f  b
-  gg
- e  c
- e  c
-  dd`;
- var digits = [
-   0x3F,0x06,0x5B,0x4F, // 0123
-   0x66,0x6D,0x7D,0x07, // 4567
-   0x7F,0x6F,0x77,0x7C, // 89Ab
-   0x39,0x5E,0x79,0x71  // cdef
- ];
- function drawCh(g,n,x,y) {
-   var b = base;
-   var d = digits[n];
-   b = b.replace(/a/g,(d&1)?"#":" ");
-   b = b.replace(/b/g,(d&2)?"#":" ");
-   b = b.replace(/c/g,(d&4)?"#":" ");
-   b = b.replace(/d/g,(d&8)?"#":" ");
-   b = b.replace(/e/g,(d&16)?"#":" ");
-   b = b.replace(/f/g,(d&32)?"#":" ");
-   b = b.replace(/g/g,(d&64)?"#":" ");
-   g.drawImage(Graphics.createImage(b),x,y);
- }
- var gr = Graphics.createArrayBuffer(H,digits.length*W+2,1,{msb:true});
- gr.setRotation(3,1);
- gr.setPixel(0,gr.getHeight()-1); // fullstop
- for (var i=0;i<digits.length;i++) drawCh(gr,i,2+i*W,0);
- gr.setRotation(0);
- var font = gr.asImage().buffer;
- var widths = E.toString([2,0,W,W,W,W,W,
-             W,W,W,W,W,
-             0,0,0,0,0,0,0,
-             W,W,W,W,W,W]);
- g.setFontCustom(font, 46, widths, H);
- g.drawString("0123456789",0,0);
- g.flip();
- print('this.setFontCustom(atob('+JSON.stringify(btoa(font))+
-       '), 46, atob('+JSON.stringify(btoa(widths))+
-       '), '+H+');');
+var W = 5; // width +1 for empty column
+var WC = 3; // width of colon
+var H = 7;
+var base = `
+aa
+f  b
+f. b
+gg
+e. c
+e  c
+dd`;
+var digits = [
+ 0x3F,0x06,0x5B,0x4F, // 0123
+ 0x66,0x6D,0x7D,0x07, // 4567
+ 0x7F,0x6F,           // 89
+ 0x80,                 // :
+ 0x77,0x7C,           // Ab
+ 0x39,0x5E,0x79,0x71  // cdef
+];
+var widths = [2,0,W,W,W,W,W,
+           W,W,W,W,W,WC,
+           0,0,0,0,0,0,
+           W,W,W,W,W,W];
+function drawCh(g,n,x,y) {
+ var b = base;
+ var d = digits[n];
+ b = b.replace(/a/g,(d&1)?"#":" ");
+ b = b.replace(/b/g,(d&2)?"#":" ");
+ b = b.replace(/c/g,(d&4)?"#":" ");
+ b = b.replace(/d/g,(d&8)?"#":" ");
+ b = b.replace(/e/g,(d&16)?"#":" ");
+ b = b.replace(/f/g,(d&32)?"#":" ");
+ b = b.replace(/g/g,(d&64)?"#":" ");
+ b = b.replace(/\./g,(d&128)?"#":" ");
+ g.drawImage(Graphics.createImage(b),x,y);
+}
+var gr = Graphics.createArrayBuffer(H,digits.length*W+2,1,{msb:true});
+gr.setRotation(3,1);
+gr.setPixel(0,gr.getHeight()-1); // fullstop
+var y = widths[0];
+for (var i=0;i<digits.length;i++) {
+ drawCh(gr,i,y,0);
+ y += (digits[i]==0x80) ? WC : W;
+}
+gr.setRotation(0);
+var font = gr.asImage().buffer;
+var widths = E.toString(widths);
+g.setFontCustom(font, 46, widths, H);
+g.drawString("0123456789:ABCDEF",20,20);
+g.flip();
+print('this.setFontCustom(atob('+JSON.stringify(btoa(font))+
+     '), 46, atob('+JSON.stringify(btoa(widths))+
+     '), '+H+');');
 */
 
 exports.add = function(graphics) {
   graphics.prototype.setFont5x7Numeric7Seg = function() {
-    this.setFontCustom(atob("AgG0GC2AAAABsADSZLAAAkyWwBgIENgDCTJDAG0mSGAAIEBsAbSZLYAwkyWwBtIkNgDYSJDAG0GCAABhIlsAbSZIAA2kSA=="), 46, atob("AgAFBQUFBQUFBQUFAAAAAAAAAAUFBQUFBQ=="), 7);
+    this.setFontCustom(atob("AgADaDBbAAAAA2ABpMlgAASZLYAwECGwBhJkhgDaTJDAAECA2ANpMlsAYSZLYAAKAG0iQ2ANhIkMAbQYIAAGEiWwBtJkgADaRIAAAAA="), 46, atob("AgAFBQUFBQUFBQUFAwAAAAAAAAUFBQUFBQ=="), 7);
   }
 }
--- modules/Font7x11Numeric7Seg.js
+++ modules/Font7x11Numeric7Seg.js
@@ -3,57 +3,67 @@
 /*
 // Magic 7 segment font maker
 var W = 7; // width +1 for empty column
+var WC = 5; // width of colon
 var H = 11;
 var base = `
  aaaa
 f    b
-f    b
-f    b
+f..  b
+f..  b
 f    b
  gggg
 e    c
-e    c
-e    c
+e..  c
+e..  c
 e    c
  dddd `;
+
 var digits = [
-  0x3F,0x06,0x5B,0x4F, // 0123
-  0x66,0x6D,0x7D,0x07, // 4567
-  0x7F,0x6F,0x77,0x7C, // 89Ab
-  0x39,0x5E,0x79,0x71  // cdef
+ 0x3F,0x06,0x5B,0x4F, // 0123
+ 0x66,0x6D,0x7D,0x07, // 4567
+ 0x7F,0x6F,           // 89
+ 0x80,                 // :
+ 0x77,0x7C,           // Ab
+ 0x39,0x5E,0x79,0x71  // cdef
 ];
+var widths = [2,0,W,W,W,W,W,
+           W,W,W,W,W,WC,
+           0,0,0,0,0,0,
+           W,W,W,W,W,W];
 function drawCh(g,n,x,y) {
-  var b = base;
-  var d = digits[n];
-  b = b.replace(/a/g,(d&1)?"#":" ");
-  b = b.replace(/b/g,(d&2)?"#":" ");
-  b = b.replace(/c/g,(d&4)?"#":" ");
-  b = b.replace(/d/g,(d&8)?"#":" ");
-  b = b.replace(/e/g,(d&16)?"#":" ");
-  b = b.replace(/f/g,(d&32)?"#":" ");
-  b = b.replace(/g/g,(d&64)?"#":" ");
-  g.drawImage(Graphics.createImage(b),x,y);
+ var b = base;
+ var d = digits[n];
+ b = b.replace(/a/g,(d&1)?"#":" ");
+ b = b.replace(/b/g,(d&2)?"#":" ");
+ b = b.replace(/c/g,(d&4)?"#":" ");
+ b = b.replace(/d/g,(d&8)?"#":" ");
+ b = b.replace(/e/g,(d&16)?"#":" ");
+ b = b.replace(/f/g,(d&32)?"#":" ");
+ b = b.replace(/g/g,(d&64)?"#":" ");
+ b = b.replace(/\./g,(d&128)?"#":" ");
+ g.drawImage(Graphics.createImage(b),x,y);
 }
 var gr = Graphics.createArrayBuffer(H,digits.length*W+2,1,{msb:true});
 gr.setRotation(3,1);
 gr.setPixel(0,gr.getHeight()-1); // fullstop
-for (var i=0;i<digits.length;i++) drawCh(gr,i,2+i*W,0);
+var y = widths[0];
+for (var i=0;i<digits.length;i++) {
+ drawCh(gr,i,y,0);
+ y += (digits[i]==0x80) ? WC : W;
+}
 gr.setRotation(0);
 var font = gr.asImage().buffer;
-var widths = E.toString([2,0,W,W,W,W,W,
-            W,W,W,W,W,
-            0,0,0,0,0,0,0,
-            W,W,W,W,W,W]);
+var widths = E.toString(widths);
 g.setFontCustom(font, 46, widths, H);
-g.drawString("0123456789",0,0);
+g.drawString("0123456789:ABCDEF",20,20);
 g.flip();
 print('this.setFontCustom(atob('+JSON.stringify(btoa(font))+
-      '), 46, atob('+JSON.stringify(btoa(widths))+
-      '), '+H+');');
+     '), 46, atob('+JSON.stringify(btoa(widths))+
+     '), '+H+');');
 */
 
 exports.add = function(graphics) {
   graphics.prototype.setFont7x11Numeric7Seg = function() {
-    this.setFontCustom(atob("ACAB70AYAwBgC94AAAAAAAAAAB7wAAPQhhDCGELwAAAAhDCGEMIXvAAeACAEAIAQPeAA8CEMIYQwhA8AB70IYQwhhCB4AAAIAQAgBAB7wAHvQhhDCGEL3gAPAhDCGEMIXvAAe9CCEEIIQPeAA94EIIQQghA8AB70AYAwBgCAAAAHgQghBCCF7wAHvQhhDCGEIAAAPehBCCEEIAAAAA=="), 46, atob("AgAHBwcHBwcHBwcHAAAAAAAAAAcHBwcHBw=="), 11);
+    this.setFontCustom(atob("ACAB70AYAwBgC94AAAAAAAAAAB7wAAPQhhDCGELwAAAAhDCGEMIXvAAeACAEAIAQPeAA8CEMIYQwhA8AB70IYQwhhCB4AAAIAQAgBAB7wAHvQhhDCGEL3gAPAhDCGEMIXvAAAAYwxgAAAPehBCCEEIHvAAe8CEEIIQQgeAA96AMAYAwBAAAADwIQQghBC94AD3oQwhhDCEAAAHvQghBCCEAAAAAAAAA="), 46, atob("AgAHBwcHBwcHBwcHBQAAAAAAAAcHBwcHBw=="), 11);
   }
 }
