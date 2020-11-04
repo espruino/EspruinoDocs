04d25a30ecad57b680440f208c5b379a2c626c7f
Gordon Williams <gw@pur3.co.uk>
2020-11-04 15:02:59 +0000

    Fix SHT3C

--- devices/SHT3C.js
+++ devices/SHT3C.js
@@ -2,7 +2,7 @@
 
 function SHT3C(_i2c) {
   this.i2c = _i2c;
-  w(0xB098); // sleep - needed after power on
+  this.w(0xB098); // sleep - needed after power on
 }
 
 /// Write a 16 bit command to the SHT3C
@@ -12,11 +12,12 @@ SHT3C.prototype.w = function(d) {
 
 /// Read with callback({humidity,temp})
 SHT3C.prototype.read = function(callback) {
-  w(0x3517); // wakeup
-  w(0x5C24); // read RH first, normal power, clock stretch
+  this.w(0x3517); // wakeup
+  this.w(0x5C24); // read RH first, normal power, clock stretch
+  var sht = this;
   setTimeout(function() {
-    var d = new DataView(i2c.readFrom(0x70,6).buffer);
-    w(0xB098); // sleep
+    var d = new DataView(sht.i2c.readFrom(0x70,6).buffer);
+    sht.w(0xB098); // sleep
     callback({ humidity : d.getUint16(0)/655.36,
                temp : d.getUint16(3)*175/65536 - 45 });
   }, 20);
