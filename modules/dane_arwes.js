69ee49e622f8ea54a2979b963d6fed0809fdf842
OmegaRogue <thatomegarogue@gmail.com>
2020-12-08 17:06:01 +0100

    Add Dane_Arwes Module

---
+++ modules/dane_arwes.js
@@ -0,0 +1,162 @@
+/* 2 */
+/*
+Graphics Functions based on the React Sci-Fi UI Framework Arwes
+*/
+
+var C = {
+  cornerSize: 14,    // description
+  cornerOffset: 3,   // description
+  borderWidth: 1     // description
+};
+
+function Arwes(cornerSize, cornerOffset, borderWidth) {
+  this.cornerSize = cornerSize;
+  this.cornerOffset = cornerOffset;
+  this.borderWidth = borderWidth;
+}
+
+
+/** 'public' constants here */
+Arwes.prototype.C = {
+  color: {
+    primary: {
+      base: "#26dafd",
+      light: "#8bebfe",
+      dark: "#029dbb"
+    },
+    secondary: {
+      base: "#df9527",
+      light: "#ecc180",
+      dark: "#8b5c15"
+    },
+    header: {
+      base: "#a1ecfb",
+      light: "#fff",
+      dark: "#3fd8f7"
+    },
+    control: {
+      base: "#acf9fb",
+      light: "#fff",
+      dark: "#4bf2f6"
+    },
+    success: {
+      base: "#00ff00",
+      light: "#6f6",
+      dark: "#090"
+    },
+    alert: {
+      base: "#ff0000",
+      light: "#f66",
+      dark: "#900"
+    },
+    disabled: {
+      base: "#999999",
+      light: "#ccc",
+      dark: "#666"
+    }
+  }
+};
+
+
+function drawTopLeftCorner(x, y) {
+  
+
+  g.setColor(this.C.color.primary.base);
+  const x1 = x - C.cornerOffset;
+  const y1 = y - C.cornerOffset;
+  g.fillRect(x1, y1, x1 + C.cornerSize, y1 + C.cornerSize);
+  g.setColor("#000000");
+  g.fillRect(x, y, x + C.cornerSize - C.cornerOffset, y + C.cornerSize - C.cornerOffset);
+}
+
+function drawTopRightCorner(x, y) {
+  
+
+  g.setColor(this.C.color.primary.base);
+  const x1 = x + C.cornerOffset;
+  const y1 = y - C.cornerOffset;
+  g.fillRect(x1, y1, x1 - C.cornerSize, y1 + C.cornerSize);
+  g.setColor("#000000");
+  g.fillRect(x, y, x - C.cornerSize - C.cornerOffset, y + C.cornerSize - C.cornerOffset);
+}
+
+function drawBottomLeftCorner(x, y) {
+  
+
+  g.setColor(this.C.color.primary.base);
+  const x1 = x - C.cornerOffset;
+  const y1 = y + C.cornerOffset;
+  g.fillRect(x1, y1, x1 + C.cornerSize, y1 - C.cornerSize);
+  g.setColor("#000000");
+  g.fillRect(x, y, x + C.cornerSize - C.cornerOffset, y - C.cornerSize + C.cornerOffset);
+}
+
+function drawBottomRightCorner(x, y) {
+  
+
+  g.setColor(this.C.color.primary.base);
+  const x1 = x + C.cornerOffset;
+  const y1 = y + C.cornerOffset;
+  g.fillRect(x1, y1, x1 - C.cornerSize, y1 - C.cornerSize);
+  g.setColor("#000000");
+  g.fillRect(x, y, x - C.cornerSize + C.cornerOffset, y - C.cornerSize + C.cornerOffset);
+}
+
+
+Arwes.prototype.drawFrame = function (x1, y1, x2, y2) {
+  
+
+  drawTopLeftCorner(x1, y1);
+  drawTopRightCorner(x2, y1);
+  drawBottomLeftCorner(x1, y2);
+  drawBottomRightCorner(x2, y2);
+  this.drawFrameTopCorners(x1, y1, x2, y2);
+}
+
+Arwes.prototype.drawFrameBottomCorners = function (x1, y1, x2, y2) {
+  
+
+  drawBottomLeftCorner(x1, y2);
+  drawBottomRightCorner(x2, y2);
+  this.drawFrameTopCorners(x1, y1, x2, y2);
+}
+
+Arwes.prototype.drawFrameTopCorners = function (x1, y1, x2, y2) {
+  
+
+  drawTopLeftCorner(x1, y1);
+  drawTopRightCorner(x2, y1);
+  this.drawFrameTopCorners(x1, y1, x2, y2);
+}
+
+Arwes.prototype.drawFrameLeftCorners = function (x1, y1, x2, y2) {
+  
+
+  drawTopLeftCorner(x1, y1);
+  drawBottomLeftCorner(x1, y2);
+  this.drawFrameTopCorners(x1, y1, x2, y2);
+}
+
+Arwes.prototype.drawFrameRightCorners = function (x1, y1, x2, y2) {
+  
+
+  drawTopRightCorner(x2, y1);
+  drawBottomRightCorner(x2, y2);
+  this.drawFrameTopCorners(x1, y1, x2, y2);
+}
+
+
+Arwes.prototype.drawFrameNoCorners = function (x1, y1, x2, y2) {
+  
+
+  g.setColor(this.C.color.primary.dark);
+  g.drawRect(x1, y1, x2, y2);
+  g.setColor("#000000");
+  g.fillRect(x1 + C.borderWidth, y1 + C.borderWidth, x2 - C.borderWidth, y2 - C.borderWidth);
+}
+
+
+/** This is 'exported' so it can be used with `require('MOD123.js').connect(pin1,pin2)` */
+exports.create = function (cornerSize = 14, cornerOffset = 3, borderWidth = 1) {
+  return new Arwes(cornerSize, cornerOffset, borderWidth);
+};
