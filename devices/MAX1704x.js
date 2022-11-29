5526f40287c567b2d3f94c9b75320dc0d5a79547
Gerrit Niezen <info@embeddedloop.com>
2022-11-29 20:51:57 +0000

    adds support for the MAX17048 and MAX17049 fuel gauges

---
+++ devices/MAX1704x.js
@@ -0,0 +1,47 @@
+/* Copyright (c) 2022 Gerrit Niezen. See the file LICENSE for copying permission. */
+/*
+Module for the MAX1704x Battery Fuel Gauge ICs
+*/
+
+function MAX1704X(_i2c) {
+  this.i2c = _i2c;
+}
+
+MAX1704X.prototype.reset = function() {
+  try {
+    this.i2c.writeTo(0x36, [0xFE, 0x54, 0x00]);
+  } catch (err) {
+    if (err.message.includes('33282')) {
+      // we got a NACK, which is what we want
+      return;
+    } else {
+      throw new Error('Something went wrong during reset()');
+    }
+  }
+};
+
+MAX1704X.prototype.readRegister = function(register) {
+  this.i2c.writeTo({address: 0x36, stop: false}, [register]);
+  var d = new DataView(this.i2c.readFrom(0x36, 3).buffer);
+  return d.getUint16(0);
+};
+
+MAX1704X.prototype.readICVersion = function() {
+  return this.readRegister(0x08).toString(16);
+};
+
+MAX1704X.prototype.readPercent = function() {
+  return this.readRegister(0x04) / 256.0;
+};
+
+MAX1704X.prototype.readVoltage = function(callback) {
+  return this.readRegister(0x02) * 78.125 / 1000000;
+};
+
+MAX1704X.prototype.readChargeRate = function(callback) {
+  return this.readRegister(0x16) * 0.208;
+};
+
+exports.connect = function (_i2c) {
+  return new MAX1704X(_i2c);
+};
