16e2d20d49787911b5be9114add56a9213667d77
brenden adamczak <cerebralasylum1@gmail.com>
2024-01-14 22:41:07 -0500

    fixed a cors problem when servering files locally

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 /* node.js script that creates a webserver to serve up the pages from the html directory */
 
 var express = require('express');
+const cors = require('cors');
+const homeDirectory = require('os').homedir();
 var app = express();
+app.use(cors());
 
 app.all('/*', function(req, res, next) {
   // The root page is part of the large site and so is not available
   // locally. Instead we default to a given page to kick off.
+  var urlLocation = __dirname + '/html';
   if (req.url === '/') {
     res.redirect('/Original.html');
     return next();
   } else if (!req.url.match(/.*\/?\./)) {
     req.url += '.html';
+  } else if (req.url.match(/^\/modules\//)){
+    console.log("got here")
+    urlLocation = homeDirectory + "/workspace/espruinowebsite/www";
   }
   console.log('returning: ' + req.url);
-  return express.static(__dirname + '/html')(req, res, next);
+  return express.static(urlLocation)(req, res, next);
 });
 
 app.listen(process.env.PORT || 3040, function(app){
