b22e604f69c90bf02da401e79d943fc490c9433f
MaBecker <mark.becker@sbit-gmbh.de>
2024-04-12 21:25:07 +0200

    add new modue hmac256
    based on hmac

---
+++ modules/hmac256.js
@@ -0,0 +1,48 @@
+/* Copyright (c) 2024 MaBe based on hmac.js from Dominik Enzinger. See the file LICENSE for copying permission. */
+function bitwiseOr0x36(b) {return b ^ 0x36; }
+function bitwiseOr0x5c(b) {return b ^ 0x5c; }
+
+/// Returns an MAC instance using the given hash. Eg. HMAC(key, require('crypto').SHA2, 64, 32)
+exports.HMAC = function(key, hash, blockSize, outputSize) {
+  if ( key.byteLength > blockSize )
+    key = hash(key);
+  this.hash = hash;
+  this.keyLength = Math.max(blockSize, key.byteLength);
+  key = new Uint8Array(key, 0, this.keyLength);
+  this.oBuf = new Uint8Array(this.keyLength + outputSize);
+  this.oBuf.set(key.map(bitwiseOr0x5c).buffer, 0);
+  this.iKeyPad = key.map(bitwiseOr0x36).buffer;
+};
+
+/// Take a message as an arraybuffer or string, return an arraybuffer
+exports.HMAC.prototype.digest = function(message) {
+  const iBuf = new Uint8Array(this.keyLength + message.byteLength);
+  iBuf.set(this.iKeyPad, 0);
+  iBuf.set(message, this.keyLength);
+  this.oBuf.set(this.hash(iBuf), this.keyLength);
+  return this.hash(this.oBuf);
+};
+
+function FixedHMAC(key, messageSize, hash, blockSize, outputSize) {
+  exports.HMAC.call(this, key, hash, blockSize, outputSize);
+  this.iBuf = new Uint8Array(this.keyLength + messageSize);
+  this.iBuf.set(this.iKeyPad, 0);
+  delete this.ikeyPad;
+};
+
+/// Take a message as an arraybuffer or string, return an arraybuffer
+FixedHMAC.prototype.digest = function(message) {
+  this.iBuf.set(message, this.keyLength);
+  this.oBuf.set(this.hash(this.iBuf), this.keyLength);
+  return this.hash(this.oBuf);
+};
+
+/// Create a basic HMAC using SHA256
+exports.SHA256 = function(key) {
+  return new exports.HMAC(key, require('crypto').SHA256, 64, 32);
+};
+
+/// FixedSHA256 is faster than SHA256, but digested message must always be the same fixed length.
+exports.FixedSHA256 = function(key, messageSize) {
+  return new FixedHMAC(key, messageSize, require('crypto').SHA256, 64, 32);
+};
